78d7bc9c6dca0953d9a075246c660d975af11cf8
mistank <milan.stankovic550@gmail.com>
2024-05-22 23:41:00 +0200

    Dodat userModal i notificationModal

--- src/components/Header/UserLog.jsx
+++ src/components/Header/UserLog.jsx
@@ -1,26 +1,40 @@
-// export default function UserLog() {
-//   return (
-//     <button className="flex flex-row log-btn items-center justify-between w-[7%]">
-//       <img className="w-6 h-6" src="/src/assets/icons/notification_bell.png" alt="notifications" />
-//       <div className="flex flex-row items-center">
-//         <img className="w-6 h-6" src="/src/assets/icons/user.png" alt="user" />
-//         <img className="w-4 h-4" src="/src/assets/icons/arrow-down.png" alt="" />
-//       </div>
-//     </button>
-//   );
+import { useState } from 'react';
+import UserModal from '../Modals/UserModal.jsx';
+import NotificationModal from '../Modals/NotificationModal.jsx';
 // }
 export default function UserLog() {
+  const [userModalIsOpen, setUserModalIsOpen] = useState(false);
+  const [notificationModalIsOpen, setNotificationModalIsOpen] = useState(false);
   return (
-    <div className="flex gap-5 justify-between w-auto">
-      <button>
-        <img className="w-6 h-6" src="/src/assets/icons/notification_bell.png" alt="notifications" />
+    <div className="relative flex gap-5 justify-between w-auto">
+      <button
+        onClick={() => {
+          setNotificationModalIsOpen((notificationModalIsOpen) => !notificationModalIsOpen);
+          if (userModalIsOpen) {
+            setUserModalIsOpen((userModalIsOpen) => !userModalIsOpen);
+          }
+        }}
+      >
+        <img className="w-6 h-6" src={`${notificationModalIsOpen ? '/src/assets/icons/notification_bell_fill.png' : '/src/assets/icons/notification_bell.png'}`} alt="notifications" />
       </button>
-      <button className="flex flex-row log-btn items-center justify-between w-[auto]">
-        <div className="flex flex-row items-center">
-          <img className="w-6 h-6" src="/src/assets/icons/user.png" alt="user" />
-          <img className="w-4 h-4" src="/src/assets/icons/arrow-down.png" alt="" />
-        </div>
-      </button>
+      <div className="flex flex-col h-auto relative">
+        <button
+          onClick={() => {
+            setUserModalIsOpen((userModalIsOpen) => !userModalIsOpen);
+            if (notificationModalIsOpen) {
+              setNotificationModalIsOpen((notificationModalIsOpen) => !notificationModalIsOpen);
+            }
+          }}
+          className="flex flex-row log-btn items-center justify-between w-[auto]"
+        >
+          <div className="flex flex-row items-center">
+            <img className="w-6 h-6" src="/src/assets/icons/user.png" alt="user" />
+            <img className={`w-4 h-4 transition-transform duration-500 ${userModalIsOpen ? 'rotate-180' : ''}`} src="/src/assets/icons/arrow-down.png" alt="" />
+          </div>
+        </button>
+        {userModalIsOpen && <UserModal isOpen={userModalIsOpen}></UserModal>}
+      </div>
+      {notificationModalIsOpen && <NotificationModal isOpen={notificationModalIsOpen}></NotificationModal>}
     </div>
   );
 }
---
+++ src/components/Modals/NotificationModal.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function UserModal({ isOpen }) {
+  return <div style={{ animation: `${isOpen ? 'fadein 0.25s' : 'fadeout 2s'}`, transition: 'transform 0.3s ease-out', transform: 'scale(1)', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.3)' }} className="absolute top-[150%] right-14  min-h-48 min-w-48 bg-[#1E1F25] rounded-md "></div>;
+}
