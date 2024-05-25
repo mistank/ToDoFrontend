39fba705f7487011c34e16b865a6b1f8533ee511
mistank <milan.stankovic550@gmail.com>
2024-05-25 13:22:41 +0200

    Responzivnost za login,signup,forgot_pass forme

--- src/components/UnloggedView/UnloggedView.jsx
+++ src/components/UnloggedView/UnloggedView.jsx
@@ -23,7 +23,7 @@ export default function UnloggedView() {
 
   return (
     <main className="flex items-center justify-center h-[100vh] bg-[#131517]">
-      <div style={{ boxShadow: '0px 0px 30px rgba(0, 0, 0, 0.3)' }} className="relative w-[30%] h-[80vh] flex items-center justify-center">
+      <div style={{ boxShadow: '0px 0px 30px rgba(0, 0, 0, 0.3)' }} className="relative w-[30%] xs:w-2/3 sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 h-[80vh] flex items-center justify-center">
         <div className="z-0 absolute top-0 left-0" style={{ animation: 'slowfloat 5s ease-in-out infinite' }}>
           <div className="transform translate-x-[-50%] translate-y-[-30%] w-48 h-48 bg-[#5051F9] rounded-full"></div>
         </div>
