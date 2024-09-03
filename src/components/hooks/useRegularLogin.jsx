2344eebc92b05d84f335f107b98f254a9ad4e4ce
mistank <milan.stankovic550@gmail.com>
2024-09-03 11:13:25 +0200

    Priprema za GHPages

--- src/components/hooks/useRegularLogin.jsx
+++ src/components/hooks/useRegularLogin.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import axios from "axios";
 import { AuthContext } from "../AuthProvider.jsx";
 import { useNavigate } from "react-router-dom";
-const apiURL = "http://127.0.0.1:8000";
+const apiURL = "https://localhost:8000";
 
 export default function useRegularLogin() {
   const { login, setUserInfo } = useContext(AuthContext);
